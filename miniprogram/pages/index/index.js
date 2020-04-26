a1b2e3f4c0e989b188d268a09a6dec5cd195ebf5
lixiaolong <lixiaolong@peicent.com>
2020-04-27 00:36:55 +0800

    登录优化

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -8,20 +8,17 @@ Page({
         loading: false,
         list: [],
         listLen: 0,
-        loginState: !!app.globalData.openid,
         showPopup: false,
         operatorList: [
-            {text: '编辑', icon: 'icon-fabu', done: 'handleEdit'},
-            {text: '分享', icon: 'icon-share', done: 'handleShare'},
-            {text: '删除', icon: 'icon-shanchu', done: 'handleDel', disabled: true}
+            { text: '编辑', icon: 'icon-fabu', done: 'handleEdit' },
+            { text: '分享', icon: 'icon-share', done: 'handleShare' },
+            { text: '删除', icon: 'icon-shanchu', done: 'handleDel', disabled: true }
         ],
     },
     // 加载触发
     onLoad: function () {
-        this.login(() => {
-            this.initStyle();
-            this.init();
-        });
+        this.initStyle();
+        this.init();
     },
     initStyle() {
         // 设置内容高度
@@ -34,7 +31,7 @@ Page({
             loading: true,
         })
         db.collection('test').get().then(res => {
-            this.setData({list: res.data});
+            this.setData({ list: res.data });
             this.setData({
                 refresherTriggered: false,
                 listLen: res.data.length,
@@ -68,83 +65,6 @@ Page({
                 loading: false,
             })
         }, 2000)
-    },
-    /**
-     * 授权登录
-     */
-    login(callback) {
-        // 调用云函数获取Openid
-        wx.showLoading({title: '正在登录中...'})
-        wx.cloud.callFunction({
-            name: 'login',
-            data: {}
-        }).then(res => {
-            let openid = res.result.openid;
-            db.collection('users').where({
-                _openid: openid
-            }).get().then(res => {
-                let user = res.data && res.data.length > 0 ? res.data[0] : null;
-                if (user) {
-                    // 更新用户登录时间
-                    db.collection('users').doc(user._id)
-                        .update({data: {lastLoginTime: new Date()}})
-                        .then(
-                            res => {
-                                this.loginSuccess(user, callback);
-                            },
-                            fail => {
-                                wx.hideLoading();
-                                wx.showToast({
-                                    title: '登录失败',
-                                    icon: 'none'
-                                })
-                            })
-                } else {
-                    // 未注册用户
-                    this.reg(openid, callback);
-                }
-            })
-        })
-    },
-    /**
-     * 注册
-     */
-    reg(openid, callback) {
-        // 获取用户信息
-        wx.getSetting({
-            success: res => {
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-                    wx.getUserInfo({
-                        success: res => {
-                            let user = res.userInfo;
-                            user.createTime = new Date();
-                            user.lastLoginTime = new Date();
-                            db.collection('users').add({
-                                data: user
-                            }).then(res => {
-                                user._id = res._id;
-                                user._openid = openid;
-                                this.loginSuccess(user, callback);
-                            }, fail => {
-                                wx.hideLoading();
-                                wx.showToast({
-                                    title: '登录失败',
-                                    icon: 'none'
-                                })
-                            })
-                        }
-                    })
-                }
-            }
-        })
-    },
-    loginSuccess(user, callback) {
-        app.globalData.openid = user._openid;
-        app.globalData.userInfo = user;
-        wx.hideLoading();
-        this.setData({loginState: true});
-        callback();
     },
     // 底部弹出框点击操作
     clickOperator(e) {
